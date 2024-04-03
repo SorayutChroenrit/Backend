701b0fdb4a0e5eea4efd6344235e058390b8335e
SorayutChroenrit <sorayutapple@gmail.com>
2024-04-03 18:06:45 +0700

    2

--- index.js
+++ index.js
@@ -19,14 +19,9 @@ const s3 = new AWS.S3({
 require("dotenv").config();
 
 app.use(cookieParser());
+app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
-const corsOptions = {
-  origin: "https://sorayutchroenrit.github.io/MangoStorage/",
-  credentials: true,
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
 
 const jwttoken = "secret";
 
